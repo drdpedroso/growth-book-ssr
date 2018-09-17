aa971fa2894c54b46b28f23942bf257938f1ef10
Eduardo <drdpedroso@gmail.com>
2018-09-17 10:47:16 +0200

    Add docker compose

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
-const dev = process.env.NODE_ENV !== "production";
-const path = require( "path" );
-const FriendlyErrorsWebpackPlugin = require( "friendly-errors-webpack-plugin" );
+const dev = process.env.NODE_ENV !== 'production'
+const path = require('path')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
-const plugins = [
-  new FriendlyErrorsWebpackPlugin(),
-];
+const plugins = [new FriendlyErrorsWebpackPlugin()]
 
 module.exports = {
-  mode: dev ? "development" : "production",
-  context: path.join( __dirname, "src" ),
-  devtool: dev ? "none" : "source-map",
+  mode: dev ? 'development' : 'production',
+  context: path.join(__dirname, 'src'),
+  target: 'node',
+  devtool: dev ? 'none' : 'source-map',
   entry: {
-    app: "./client.js",
+    index: './index.js'
   },
   resolve: {
-    modules: [
-      path.resolve( "./src" ),
-      "node_modules",
-    ],
+    modules: [path.resolve('./src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)?$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader",
-      },
-    ],
+        loader: 'babel-loader'
+      }
+    ]
   },
   output: {
-    path: path.resolve( __dirname, "dist" ),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
   },
-  plugins,
-};
+  plugins
+}
